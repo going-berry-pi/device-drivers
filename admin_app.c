41a9ac2fc40960918bde44b620a03907b79b7f10
younho9 <younho9.choo@gmail.com>
2020-06-25 16:11:35 +0100

    feat: Update administrator app

--- admin_app.c
+++ admin_app.c
@@ -10,6 +10,11 @@
 
 #define BUFSIZE         512
 #define MAX_NAMELEN     32
+#define MAX_STUDENTS    9
+
+#define SEAT_PORT       9001
+#define MOTOR_PORT      9002
+#define ENTRANCE_PORT   9003
 
 typedef struct {
     int command;
@@ -18,20 +23,27 @@ typedef struct {
 
 typedef struct {
     int student_number;
+    float temp;
+    int name_len;
     char name[MAX_NAMELEN];
-    float student_temp;
-} STUDENT;
+    int seat_number;
+} STUDENT_DATA;
 
 typedef struct {
     int seat_number;
     int status;
 } SEAT_DATA;
 
+STUDENT_DATA students[MAX_STUDENTS];
+int student_count = 0;
+
 int SEAT_MAP[3][3] = {
     {0, 0, 0},
     {0, 0, 0},
     {0, 0, 0}
 };
+
+int isUpdated = 0;
 
 void *motor_client() {
     int sock;
@@ -50,11 +62,13 @@ void *motor_client() {
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr("192.168.1.3");
-    server_addr.sin_port = htons(9003);
+    server_addr.sin_port = htons(MOTOR_PORT);
 
     if (connect(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1) {
+        printf("[창문 서버와 연결이 실패하였습니다.]\n");
+        close(sock);
+        return (void *)0;
         perror("connect() error\n");
-        exit(0);
     }
 
     printf("[창문 서버와 연결되었습니다.]\n");
@@ -126,6 +140,9 @@ void *entrance_server() {
     int addrlen, retval, msglen, offset;
     
     char buf[BUFSIZE + 1];
+    int seat_count = 1;
+
+    STUDENT_DATA student_buffer;
 
     listen_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (listen_sock == -1) {
@@ -136,7 +153,7 @@ void *entrance_server() {
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(9000);
+    server_addr.sin_port = htons(ENTRANCE_PORT);
 
     if (bind(listen_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
         perror("bind() error\n");
@@ -168,8 +185,26 @@ void *entrance_server() {
                 continue;
 
             buf[msglen] = '\0';
+            offset = 0;
 
-            printf("[TCP/%s:%d] %s\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port), buf);
+            memcpy(&(student_buffer.student_number), buf + offset, sizeof(int));
+            offset += sizeof(int);
+        
+            memcpy(&(student_buffer.temp), buf + offset, sizeof(float));
+            offset += sizeof(float);
+
+            memcpy(&(student_buffer.name_len), buf + offset, sizeof(int));
+            offset += sizeof(int);
+
+            memcpy(&(student_buffer.name), buf + offset, sizeof(char) * student_buffer.name_len + 1);
+
+            printf("[입실 서버] 학생 정보를 받았습니다.\n");
+
+            student_buffer.seat_number = seat_count;
+
+            students[student_count++] = student_buffer;
+            isUpdated = 1;
+            seat_count += 2;
         }
         close(client_sock);
     }
@@ -200,7 +235,7 @@ void *seat_server() {
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(9001);
+    server_addr.sin_port = htons(SEAT_PORT);
 
     if (bind(listen_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
         perror("bind() error\n");
@@ -233,19 +268,16 @@ void *seat_server() {
 
             offset = 0;
 
-		    memcpy(&(seat_data.seat_number), buf + offset, sizeof(int));
-		    offset += sizeof(int);
-		
-		    memcpy(&(seat_data.status), buf + offset, sizeof(int));
+            memcpy(&(seat_data.seat_number), buf + offset, sizeof(int));
+            offset += sizeof(int);
+        
+            memcpy(&(seat_data.status), buf + offset, sizeof(int));
 
             printf("[좌석 클라이언트 %d] %d번 좌석의 상태가 %s으로 바뀌었습니다.\n", ntohs(client_addr.sin_port), seat_data.seat_number, seat_status[seat_data.status]);
             
             SEAT_MAP[(seat_data.seat_number - 1) / 3][(seat_data.seat_number - 1) % 3] = seat_data.status;
             
-            printf("\n좌석 상태\n");
-            printf("[%s] [%s] [%s]\n", seat_status[SEAT_MAP[0][0]], seat_status[SEAT_MAP[0][1]], seat_status[SEAT_MAP[0][2]]);
-            printf("[%s] [%s] [%s]\n", seat_status[SEAT_MAP[1][0]], seat_status[SEAT_MAP[1][1]], seat_status[SEAT_MAP[1][2]]);
-            printf("[%s] [%s] [%s]\n\n", seat_status[SEAT_MAP[2][0]], seat_status[SEAT_MAP[2][1]], seat_status[SEAT_MAP[2][2]]);
+            isUpdated = 1;
         }
         close(client_sock);
     }
@@ -257,6 +289,7 @@ int main(int argc, char* argv[]) {
     pthread_t entrance_t, seat_t, motor_t;
     int entrance_t_result, seat_t_result, motor_t_result;
 
+    int isFirst = 1;
     char* seat_status[] = {"비움", "착석"};
 
     if (pthread_create(&entrance_t, NULL, entrance_server, NULL) < 0) {
@@ -275,6 +308,19 @@ int main(int argc, char* argv[]) {
     }
 
     while(1) {
+        if(isUpdated || isFirst) {
+            printf("\n좌석 상태\n");
+            printf("[%s] [%s] [%s]\n", seat_status[SEAT_MAP[0][0]], seat_status[SEAT_MAP[0][1]], seat_status[SEAT_MAP[0][2]]);
+            printf("[%s] [%s] [%s]\n", seat_status[SEAT_MAP[1][0]], seat_status[SEAT_MAP[1][1]], seat_status[SEAT_MAP[1][2]]);
+            printf("[%s] [%s] [%s]\n\n", seat_status[SEAT_MAP[2][0]], seat_status[SEAT_MAP[2][1]], seat_status[SEAT_MAP[2][2]]);
+
+            for(int i = 0; i<student_count; i++) {
+                printf("[%d번 좌석] 학번 : %d, 온도 : %f, 이름 길이 : %d, 이름 : %s\n", students[i].seat_number, students[i].student_number, students[i].temp, students[i].name_len, students[i].name);
+            }
+            
+            isUpdated = 0;
+            isFirst = 0;
+        }
         sleep(5);
     }
 
